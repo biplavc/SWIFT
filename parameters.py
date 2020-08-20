d447a364454c7d9cbca3ead46c4601be3bbf1bd1
Biplav Choudhury <biplav93@gmail.com>
2020-08-19 23:51:00 -0400

    delay return added

--- parameters.py
+++ parameters.py
@@ -2,9 +2,9 @@ import numpy as np
 import math
 import random
 
-n_devices          = 3
+num_devices          = 3
 packet_size        = 50
-sample_sizes       = packet_size * np.random.randint(1, 5, size=n_devices) # bits
+sample_sizes       = packet_size * np.random.randint(1, 5, size=num_devices) # bits
 # print(sample_sizes)
 packets_per_sample = [math.ceil(x*1.00/packet_size) for x in sample_sizes]
 # print(packets_per_sample)
--- test.c
+++ test.c
@@ -1,57 +1,72 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include<conio.h>
+// #include<conio.h>
 
 // int test_cfun_(int *arg)
 // #FUNCTION INTEGER TEST_CFUN
 #define MAX 100 
 
-FILE *fp;
+// FILE *fp;
 
 // char a[100];
 float d;
+
+int num_devices = 3; // new
 
 float retcod;
 float b;
 char cmdbuf[80];
 
-int testv_(double *time,int *x)
+int testv_(double *times, double *x) // new - time and x are arrays of dimension - num_devices*1
 {
 
-float i;
-FILE *fp;
-char a[100];
-//double d;
-// double i;
-//double z = x;
+    float i;
+    FILE *fp;
+    char a[100];
+    //double d;
+    // double i;
+    //double z = x;
 
 
-fp = fopen("test.txt", "w");
-fprintf(fp, "%d",*x);
-fclose(fp);
+    fp = fopen("test.txt", "w");
+    for (int i=0; i<num_devices; i++)
+    {
+        fprintf(fp, "%d", int(*(times+i)));
+        fprintf(fp, "\n");
+    }
+    for (int i=0; i<num_devices; i++)
+    {
+        fprintf(fp, "%d", int(*(x+i)));
+        fprintf(fp, "\n");
+    }
+    
+    fclose(fp);
 
 
 
 
-d= system("C:\\Users\\rahul\\OneDrive\\Desktop\\FORTRAN\\dist\\random_num.exe");
-fp = fopen("result.txt", "r");
-fscanf(fp, "%s", a);
+    // d= system("C:\\Users\\rahul\\OneDrive\\Desktop\\FORTRAN\\dist\\random_num.exe");
+    // d= system("/home/biplav/SWIFT/main.exe");
+    FILE *fileStream;
+    char fileText [100];
 
-sscanf(a, "%f", &i);
-fclose(fp);
-//printf("%f",i);
-
-
-return (i);
+    fileStream = fopen ("result.txt", "r"); 
+    fgets (fileText, 100, fileStream);
+    printf("%s", fileText);
+    fclose(fileStream);
 }
 
 
-//int main()
-//{
-//float p;
+int main()
+{
+float p;
 // double *a=0.1, *b=.1, *c;
 // printf("b");
-//p=testv_(0.1,0.1);
-//printf("%f",p);
-//}
+double foo [3] = {2, 2, 1};
+double foo1 [3] = { 0, 0, 0};
+
+// p=testv_(0.1,0.1);
+p=testv_(foo,foo1);
+// printf("%f",p);
+}
 
