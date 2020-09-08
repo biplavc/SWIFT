72e190f40f1df1b69932ceeaa8c0a7dbc04f8216
Biplav Choudhury <biplav93@gmail.com>
2020-09-08 12:48:12 -0400

    Realistic scheduler updated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SWIFT
+# PCCAD - Communications Interface
 python codes for channel simulation
 '''
 Code will interface with PSCAD power simulation software so as to provide the channel condition and the resulting delay, PER to the communicating entities. With the delay and PER, PSCAD will operate and the stability of the smart grid will be investigated.
--- parameters.py
+++ parameters.py
@@ -3,7 +3,7 @@ import math
 import random
 
 num_devices          = 3
-packet_size        = 50
+packet_size        = 64 # bits
 sample_sizes       = packet_size * np.random.randint(1, 5, size=num_devices) # bits
 # print(sample_sizes)
 packets_per_sample = [math.ceil(x*1.00/packet_size) for x in sample_sizes]
@@ -13,4 +13,6 @@ packets_per_sample = [math.ceil(x*1.00/packet_size) for x in sample_sizes]
 total_packets      = np.sum(packets_per_sample)
 sample_priority    = np.array([1,1,1])
 number_of_RBs      = 3
-throughput_per_RB  = 10**6 # 1 Mbps
+modulation_orders = [2,4,6,8]
+base_throughput = 1.07 # in Mbps
+throughputs = [base_throughput, 2*base_throughput, 3*base_throughput, 4*base_throughput]
